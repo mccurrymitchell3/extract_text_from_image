031f36a0292a1a24a78b54e3db40784ff0e72b49
mccurrymitchell3 <mccurrymitchell@gmail.com>
2020-05-16 13:42:50 -0400

    added paragraph text extraction

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import pytesseract
 
 pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-img = cv2.imread("quote.jpg")
+img = cv2.imread("paragraph.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 blur = cv2.GaussianBlur(gray, (3,3), 0)
-thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 205, 1)
+thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 5, 1)
 
-rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 5))
 dilation = cv2.dilate(thresh, rect_kernel, iterations = 1)
+
 
 contours, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
@@ -20,7 +21,13 @@ file = open("recognized.txt", "w+")
 file.write("")
 file.close()
 
-for cnt in contours: 
+cv2.drawContours(dilation, contours, -1, (0, 255, 0), 3) 
+  
+cv2.imshow('Contours', dilation) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows() 
+
+for cnt in contours:
     x, y, w, h = cv2.boundingRect(cnt) 
       
     # Drawing a rectangle on copied image 

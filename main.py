58591d54417d623de1e9b2e639bc13392c7c5688
mccurrymitchell3 <mccurrymitchell@gmail.com>
2020-05-16 01:44:20 -0400

    text extraction

---
+++ main.py
@@ -0,0 +1,43 @@
+import cv2
+import pytesseract
+
+pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+img = cv2.imread("quote.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+blur = cv2.GaussianBlur(gray, (3,3), 0)
+thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 205, 1)
+
+rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+dilation = cv2.dilate(thresh, rect_kernel, iterations = 1)
+
+contours, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+im2 = img.copy()
+
+file = open("recognized.txt", "w+")
+file.write("")
+file.close()
+
+for cnt in contours: 
+    x, y, w, h = cv2.boundingRect(cnt) 
+      
+    # Drawing a rectangle on copied image 
+    rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2) 
+      
+    # Cropping the text block for giving input to OCR 
+    cropped = im2[y:y + h, x:x + w] 
+      
+    # Open the file in append mode 
+    file = open("recognized.txt", "a") 
+      
+    # Apply OCR on the cropped image 
+    text = pytesseract.image_to_string(cropped) 
+      
+    # Appending the text into file 
+    file.write(text) 
+    file.write("\n") 
+      
+    # Close the file 
+    file.close 
